1bbe8fa5ce31e9ba515ad82efc48c8ba5ad8dc28
Quame <anoldanyor@gmail.com>
2020-05-24 14:19:12 +0000

    Achievement page UI done

--- TranSafe/src/environments/environment.ts
+++ TranSafe/src/environments/environment.ts
@@ -5,14 +5,14 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: process.env.Transafe_api_key,
-    authDomain: process.env.Transafe_authdomain,
-    databaseURL: process.env.Transafe_database_url,
-    projectId: process.env.Transafe_project_id,
-    storageBucket: process.env.Transafe_storage_bucket,
-    messagingSenderId: process.env.Transafe_messaging_sender_id,
-    appId: process.env.Transafe_app_id,
-    measurementId: process.env.Transafe_measurement_id
+    // apiKey: process.env.Transafe_api_key,
+    // authDomain: process.env.Transafe_authdomain,
+    // databaseURL: process.env.Transafe_database_url,
+    // projectId: process.env.Transafe_project_id,
+    // storageBucket: process.env.Transafe_storage_bucket,
+    // messagingSenderId: process.env.Transafe_messaging_sender_id,
+    // appId: process.env.Transafe_app_id,
+    // measurementId: process.env.Transafe_measurement_id
   }
 
 };
