e7afa8be06389c5ee83a6cb8dc82dfdaaef30fa8
ian akoto <iancecilakoto@gmail.com>
2020-05-29 18:26:32 +0000

    home navigation and achievement layout styled and routes implemented

--- TranSafe/src/app/app.component.ts
+++ TranSafe/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+import { MenuController } from '@ionic/angular';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -13,7 +13,8 @@ export class AppComponent {
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    private menuCtrl: MenuController
   ) {
     this.initializeApp();
   }
@@ -24,4 +25,9 @@ export class AppComponent {
       this.splashScreen.hide();
     });
   }
+
+  toggleMenu() {
+    this.menuCtrl.close('main-menu');
+  }
+
 }
