5435c1e46acf128c8365f84cbd2a7498ddf8d62d
Michael Hammond <mickeymond@gmail.com>
2020-06-01 23:58:46 +0100

    Getting Started #7

--- TranSafe/src/app/app.component.ts
+++ TranSafe/src/app/app.component.ts
@@ -4,6 +4,9 @@ import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { MenuController } from '@ionic/angular';
+
+import { UserSpeedingService } from './services/user-speeding.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
@@ -14,9 +17,11 @@ export class AppComponent {
     private platform: Platform,
     private splashScreen: SplashScreen,
     private statusBar: StatusBar,
-    private menuCtrl: MenuController
+    private menuCtrl: MenuController,
+    private userSpeeding: UserSpeedingService
   ) {
     this.initializeApp();
+    this.userSpeeding.watchPosition();
   }
 
   initializeApp() {
