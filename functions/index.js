774ff374b50779b72af167f2ab75010aab370f56
Michael Hammond <mickeymond@gmail.com>
2020-06-05 07:49:06 +0100

    Reducing to only notifyUserOfOverSpeeding function #9

--- functions/index.js
+++ functions/index.js
@@ -1,33 +1,23 @@
 // The Cloud Functions for Firebase SDK to create Cloud Functions and setup triggers.
 const functions = require('firebase-functions');
+// const https = require('https');
 
 // The Firebase Admin SDK to access Cloud Firestore.
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-// Function to Compute and Store User OverSpeeding
-exports.computeAndStoreOverSpeeding = functions.https.onCall((data, context) => {
-  // Extreact Client Data
+exports.notifyUserOfOverSpeeding = functions.firestore
+  .document('speeding/{timestamp}')
+  .onWrite((change, context) => {
+    console.log(change);
+    // Extreact Client Data
 
-  // Check If Client is Authenticated
+    // Check If Client is Authenticated
 
-  // Compute Over Speeding
+    // Retrieve fcmToken for User from Database
 
-  // Store Results
+    // Push Notification to User
 
-  // Return Response
-  return { message: 'Operation Completed Successfully' };
-});
-
-exports.notifyUserOfOverSpeending = functions.https.onCall((data, context) => {
-  // Extreact Client Data
-
-  // Check If Client is Authenticated
-
-  // Retrieve fcmToken for User from Database
-
-  // Push Notification to User
-
-  // Return Response
-  return { message: 'Operation Completed Successfully' };
-});
+    // Return Response
+    return;
+  });
